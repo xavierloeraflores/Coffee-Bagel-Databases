c7574f071ab688bdb56047d60200039cf611d586
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-09-12 06:41:15 -0700

    FEAT: b2

---
+++ part-b/b2.sql
@@ -0,0 +1,23 @@
+INSERT INTO Coffee_Shop
+VALUES
+	(1,	'Starbucks Coffee', 'San Jose', 'CA'),
+	(2,	'Pete Coffee', 'Los Angeles', 'CA'),
+    (3,	'Phil Coffee',	'Brooklyn', 'NY');
+    
+INSERT INTO Employee
+VALUES
+	(1,	'John', 'Wick', '2023-09-10', 'Barista', 1),
+	(2,	'Indiana', 'Jones', '2023-10-09', 'Manager', 2),
+    (3,	'Sherlock',	'Holmes', '2023-07-12', 'Driver', 3);
+
+INSERT INTO Supplier
+VALUES
+	(1,	'CoffeeBeans', 'United States', 'Michael West',	'michael@coffeebeans.com'),
+	(2,	'ValueBeans', 'United States', 'John Simmons', 'john@valuebeans.com'),
+    (3,	'LuxBeans', 'United States', 'Mary Strong', 'mary@luxbeans.com');
+    
+INSERT INTO Coffee
+VALUES
+	(1, 1, 1, 'Mocha', 7.99),
+	(2, 2, 2, 'Frap', 5.99),
+    (3, 3, 3, 'Black', 1.50);
